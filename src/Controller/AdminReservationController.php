a8abc9c7e36d9a6a45260f718848976220a0d8b2
Youssef Ben Salem <youssefbselem@gmail.com>
2026-03-29 21:09:03 +0100

    Refactor reservations display to group by event and enhance UI with card layout

--- src/Controller/AdminReservationController.php
+++ src/Controller/AdminReservationController.php
@@ -5,6 +5,7 @@ use App\Repository\ReservationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\Common\Collections\ArrayCollection;
 
 #[Route('/admin')]
 class AdminReservationController extends AbstractController
@@ -12,10 +13,23 @@ class AdminReservationController extends AbstractController
     #[Route('/reservations', name: 'admin_reservations')]
     public function reservations(ReservationRepository $reservationRepository): Response
     {
-        $reservations = $reservationRepository->findAll();
+        $reservations = $reservationRepository->findBy([], ['createdAt' => 'DESC']);
+
+        $grouped = [];
+        foreach ($reservations as $reservation) {
+            $event = $reservation->getEvent();
+            $eventId = $event?->getId() ?? 0;
+            if (!isset($grouped[$eventId])) {
+                $grouped[$eventId] = [
+                    'event' => $event,
+                    'items' => [],
+                ];
+            }
+            $grouped[$eventId]['items'][] = $reservation;
+        }
 
         return $this->render('admin/reservations.html.twig', [
-            'reservations' => $reservations,
+            'grouped_reservations' => $grouped,
         ]);
     }
 }
