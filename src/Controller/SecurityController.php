392907077b3ab5bc2f55b30cdeceeb0fd4295130
Youssef Ben Salem <youssefbselem@gmail.com>
2026-03-29 19:58:19 +0100

    Implement user registration functionality with validation and error handling

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,6 +5,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class SecurityController extends AbstractController
 {
@@ -28,8 +32,42 @@ class SecurityController extends AbstractController
     public function logout(): void {}
 
     #[Route('/register', name: 'app_register')]
-    public function register(): Response
-    {
-        return $this->render('security/register.html.twig');
+public function register(
+    Request $request,
+    UserPasswordHasherInterface $passwordHasher,
+    EntityManagerInterface $entityManager
+): Response {
+    $error = null;
+    $lastUsername = null;
+
+    if ($request->isMethod('POST')) {
+        $lastUsername = $request->request->get('username', '');
+        $submittedToken = $request->request->get('_csrf_token');
+
+        if (!$this->isCsrfTokenValid('register', $submittedToken)) {
+            $error = 'Invalid session, please try again.';
+        } elseif ($lastUsername === '' || $request->request->get('password') === '') {
+            $error = 'Please provide a username and password.';
+        } elseif ($entityManager->getRepository(User::class)->findOneBy(['username' => $lastUsername])) {
+            $error = 'Username is already taken.';
+        } else {
+            $user = new User();
+            $user->setUsername($lastUsername);
+            $user->setPassword(
+                $passwordHasher->hashPassword($user, $request->request->get('password'))
+            );
+            $user->setRoles(['ROLE_USER']);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
     }
+
+    return $this->render('security/register.html.twig', [
+        'error' => $error,
+        'last_username' => $lastUsername,
+    ]);
+}
 }
