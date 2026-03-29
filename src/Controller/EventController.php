02b4d8938de2072ffd8965a31fc1d8c4eb0c1935
Youssef Ben Salem <youssefbselem@gmail.com>
2026-03-29 20:45:58 +0100

    Implement event reservation functionality with form handling and event detail display

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -1,8 +1,13 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Event;
+use App\Entity\Reservation;
+use App\Form\ReservationType;
 use App\Repository\EventRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,6 +20,29 @@ class EventController extends AbstractController
 
         return $this->render('events/index.html.twig', [
             'events' => $events,
+        ]);
+    }
+
+    #[Route('/events/{id}', name: 'app_event_show', methods: ['GET', 'POST'], requirements: ['id' => '\\d+'])]
+    public function show(Event $event, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $reservation = new Reservation();
+        $form = $this->createForm(ReservationType::class, $reservation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $reservation->setEvent($event);
+            $entityManager->persist($reservation);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Réservation enregistrée.');
+
+            return $this->redirectToRoute('app_event_show', ['id' => $event->getId()]);
+        }
+
+        return $this->render('events/show.html.twig', [
+            'event' => $event,
+            'form' => $form->createView(),
         ]);
     }
 }
