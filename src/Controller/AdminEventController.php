5db2841c648a761d77c9dfbed1249f61a60b272a
Youssef Ben Salem <youssefbselem@gmail.com>
2026-03-29 20:33:35 +0100

    Implement CRUD functionality for events, including creation, editing, deletion, and display of event details in the admin panel

--- src/Controller/AdminEventController.php
+++ src/Controller/AdminEventController.php
@@ -1,21 +1,92 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Event;
+use App\Form\EventType;
 use App\Repository\EventRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/admin')]
+#[Route('/admin/events')]
 class AdminEventController extends AbstractController
 {
-    #[Route('/events', name: 'admin_events')]
-    public function events(EventRepository $eventRepository): Response
+    #[Route('', name: 'admin_events', methods: ['GET'])]
+    public function index(EventRepository $eventRepository): Response
     {
         $events = $eventRepository->findAll();
 
         return $this->render('admin/events.html.twig', [
             'events' => $events,
         ]);
+    }
+
+    #[Route('/new', name: 'admin_events_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $event = new Event();
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($event);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Event created.');
+
+            return $this->redirectToRoute('admin_events');
+        }
+
+        return $this->render('admin/event_form.html.twig', [
+            'form' => $form->createView(),
+            'is_edit' => false,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'admin_events_show', methods: ['GET'], requirements: ['id' => '\\d+'])]
+    public function show(Event $event): Response
+    {
+        return $this->render('admin/event_show.html.twig', [
+            'event' => $event,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'admin_events_edit', methods: ['GET', 'POST'], requirements: ['id' => '\\d+'])]
+    public function edit(Request $request, Event $event, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Event updated.');
+
+            return $this->redirectToRoute('admin_events');
+        }
+
+        return $this->render('admin/event_form.html.twig', [
+            'form' => $form->createView(),
+            'is_edit' => true,
+        ]);
+    }
+
+    #[Route('/{id}/delete', name: 'admin_events_delete', methods: ['POST'], requirements: ['id' => '\\d+'])]
+    public function delete(Request $request, Event $event, EntityManagerInterface $entityManager): RedirectResponse
+    {
+        $submittedToken = $request->request->get('_token');
+
+        if ($this->isCsrfTokenValid('delete_event_' . $event->getId(), $submittedToken)) {
+            $entityManager->remove($event);
+            $entityManager->flush();
+            $this->addFlash('success', 'Event deleted.');
+        } else {
+            $this->addFlash('error', 'Invalid delete request.');
+        }
+
+        return $this->redirectToRoute('admin_events');
     }
 }
---
+++ src/Form/EventType.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('date', DateTimeType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('location', TextType::class)
+            ->add('seats', IntegerType::class)
+            ->add('image', TextType::class, [
+                'required' => false,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
